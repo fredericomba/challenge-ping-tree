2d1e4b64b017a0abe7692e3e8733ee4ab9186b15
Frederico Alves <frederico.miranda.brandao@gmail.com>
2021-10-13 21:11:08 -0400

    Add test for '[POST] /api/target/:id' route.

--- test/endpoints.js
+++ test/endpoints.js
@@ -54,3 +54,49 @@ test.serial.cb('[GET] /api/target/1', function (t) {
       t.end()
     })
 })
+
+test.serial.cb('[POST] /api/target/2', function (t) {
+  var url = '/api/target/2'
+  var optionsPost = {
+    method: 'POST',
+    encoding: 'json'
+  }
+  var postBody = {
+    // notice the absence of the 'id' property
+    url: 'http://example.com',
+    value: '0.50',
+    maxAcceptsPerDay: '10',
+    accept: {
+      geoState: {
+        $in: ['ca', 'ny']
+      },
+      hour: {
+        $in: ['13', '14', '15']
+      }
+    }
+  }
+  var referenceBody = {
+    id: '2',
+    ...postBody
+  }
+  var stream = servertest(server(), url, optionsPost, function (err, res) {
+    t.falsy(err, 'no error')
+    var { headers, statusCode, body } = res
+    t.is(headers['content-type'], 'application/json', '"Content-Type" must be "application/json"')
+    t.is(statusCode, 200)
+    t.is(typeof body === 'object', true, 'body should be an object')
+    t.deepEqual(body, referenceBody, 'body should be deeply equal to referenceBody')
+    redis.loadTarget(body.id)
+      .then((json) => {
+        var body = JSON.parse(json)
+        t.deepEqual(body, referenceBody, 'body should be deeply equal to referenceBody')
+        t.end()
+      })
+      .catch((err) => {
+        t.fail(err.toString())
+        t.end()
+      })
+  })
+  stream.write(JSON.stringify(postBody))
+  stream.end()
+})
