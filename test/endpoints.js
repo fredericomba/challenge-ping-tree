b985a50403022f00fca2b488f35fefbcdc7f81a9
Frederico Alves <frederico.miranda.brandao@gmail.com>
2021-10-13 20:31:08 -0400

    Add test for '[GET] /api/target/:id' route.

--- test/endpoints.js
+++ test/endpoints.js
@@ -4,6 +4,7 @@ var test = require('ava')
 var servertest = require('servertest')
 
 var server = require('../lib/server')
+var redis = require('../lib/redis')
 
 test.serial.cb('healthcheck', function (t) {
   var url = '/health'
@@ -14,4 +15,42 @@ test.serial.cb('healthcheck', function (t) {
     t.is(res.body.status, 'OK', 'status is ok')
     t.end()
   })
+})
+
+test.serial.cb('[GET] /api/target/1', function (t) {
+  var url = '/api/target/1'
+  var optionsGet = {
+    method: 'GET',
+    encoding: 'json'
+  }
+  var referenceBody = {
+    id: '1',
+    url: 'http://example.com',
+    value: '0.50',
+    maxAcceptsPerDay: '10',
+    accept: {
+      geoState: {
+        $in: ['ca', 'ny']
+      },
+      hour: {
+        $in: ['13', '14', '15']
+      }
+    }
+  }
+  redis.saveTarget(referenceBody)
+    .then(() => {
+      servertest(server(), url, optionsGet, function (err, res) {
+        t.falsy(err, 'no error')
+        var { headers, statusCode, body } = res
+        t.is(headers['content-type'], 'application/json', '"Content-Type" must be "application/json"')
+        t.is(statusCode, 200)
+        t.is(typeof body === 'object', true, 'body should be an object')
+        t.deepEqual(body, referenceBody, 'body should be deeply equal to referenceBody')
+        t.end()
+      })
+    })
+    .catch((err) => {
+      t.fail(err.toString())
+      t.end()
+    })
 })
